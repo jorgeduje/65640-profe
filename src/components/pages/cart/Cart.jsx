69c9acec9f7e3a6580310c17376aabf718e129df
jorgeduje <jorgeduje4@gmail.com>
2024-11-25 22:26:47 -0300

    se trabaja con context 2

--- src/components/pages/cart/Cart.jsx
+++ src/components/pages/cart/Cart.jsx
@@ -3,26 +3,39 @@ import { Link } from "react-router-dom";
 import { CartContext } from "../../../context/CartContext";
 
 const Cart = () => {
-  const { cart } = useContext(CartContext);
+  const { cart, resetCart, removeProduct, getTotalPrice } =
+    useContext(CartContext);
+  let totalAmount = getTotalPrice();
   return (
     <div>
       <h1>pagina carrito</h1>
-      {cart.map((elemento) => {
-        return (
-          <div
-            key={elemento.id}
-            style={{ border: "2px solid black", padding: "20px" }}
-          >
-            <h3>{elemento.title}</h3>
-            <h3>cantidad: {elemento.quantity}</h3>
-            <h3>precio: {elemento.price}</h3>
-            <button>Eliminar</button>
-          </div>
-        );
-      })}
+      {cart.length > 0 ? (
+        cart.map((elemento) => {
+          return (
+            <div
+              key={elemento.id}
+              style={{ border: "2px solid black", padding: "20px" }}
+            >
+              <h3>{elemento.title}</h3>
+              <h3>cantidad: {elemento.quantity}</h3>
+              <h3>precio: {elemento.price}</h3>
+              <button onClick={() => removeProduct(elemento.id)}>
+                Eliminar
+              </button>
+            </div>
+          );
+        })
+      ) : (
+        <h2>El carrrito esta vacio</h2>
+      )}
 
-      <button>Limpiar todo el carrito</button>
-      <Link to="/checkout">Finalizar compra</Link>
+      {cart.length > 0 && (
+        <div>
+          <h3>El total del carrito es : ${totalAmount}</h3>
+          <button onClick={resetCart}>Limpiar todo el carrito</button>
+          <Link to="/checkout">Finalizar compra</Link>
+        </div>
+      )}
     </div>
   );
 };
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -3,12 +3,28 @@ import { createContext, useState } from "react";
 export const CartContext = createContext();
 
 export const CartContextProvider = ({ children }) => {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState([]); // {3} {5} {4} --> 12
 
   const addToCart = (product) => {
     setCart([...cart, product]);
   };
 
-  let data = { cart, addToCart };
+  const resetCart = () => {
+    setCart([]);
+  };
+
+  const removeProduct = (id) => {
+    let filteredArray = cart.filter((elemento) => elemento.id !== id);
+    setCart(filteredArray);
+  };
+
+  const getTotalPrice = () => {
+    let total = cart.reduce((total, elemento) => {
+      return total + elemento.price * elemento.quantity;
+    }, 0);
+    return total;
+  };
+
+  let data = { cart, addToCart, resetCart, removeProduct, getTotalPrice };
   return <CartContext.Provider value={data}>{children}</CartContext.Provider>;
 };
